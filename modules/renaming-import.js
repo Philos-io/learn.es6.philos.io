5030619a338a9657448cda137cb12a20f0da7025
davy engone <davy@philos.io>
2015-09-27 21:54:56 +0200

    removed few modules

--- modules/renaming-import.js
+++
@@ -1,4 +0,0 @@
-// Renaming import: Avoiding name collision
-import _ from 'lodash';
-import _ from './export.js';
-
--- test/classes.spec.js
+++ test/classes.spec.js
@@ -1,9 +1,16 @@
-import User from '../classes/user;
+//import User from '../classes/user;
+import chai from 'chai';
+
+
 
 describe('User class', () => {
-	
+
 	it('should have name', () =>{
-		
-			
+
+		let user = new User();
+
+		expect(2).to.equal(2);
+
+
 	});
 });
